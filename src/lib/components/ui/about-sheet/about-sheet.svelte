598af331262eb4704722946d111ac057d3f98a83
Sujit Kumar <60378235+therealsujitk@users.noreply.github.com>
2026-01-06 20:22:40 +0530

    feat: add a sheet containg the app description and useful links

---
+++ src/lib/components/ui/about-sheet/about-sheet.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import { Button } from "$lib/components/ui/button/index.js";
+  import * as Sheet from "$lib/components/ui/sheet/index.js";
+  import { buttonVariants } from "$lib/components/ui/button/index.js";
+  import Info from '@lucide/svelte/icons/info';
+  import GitHub from '@lucide/svelte/icons/github'
+  import ContactRound from '@lucide/svelte/icons/contact-round';
+
+</script>
+ 
+<Sheet.Root>
+  <Sheet.Trigger class={[buttonVariants({ variant: "outline", size: "icon" }), "cursor-pointer"]}>
+    <Info />
+  </Sheet.Trigger>
+  <Sheet.Content side="right">
+    <Sheet.Header>
+      <Sheet.Title>About CP Contests</Sheet.Title>
+    </Sheet.Header>
+    <div class="grid flex-1 auto-rows-min gap-6 mt-[-10px] px-4">
+      <span>Hi, I'm <b>@therealsujitk</b> 👋 This app brings all upcoming contests into one clean place, with an easy way to add them directly to your calendar 📅. Hope you find it useful 🚀</span>
+
+      <div class="flex flex-col gap-3">
+        <span>Here are some links that may interest you 👀</span>
+
+        <Button variant="secondary" href="https://github.com/therealsujitk/cp-contests" target="_blank">
+          <GitHub /> GitHub Repository
+        </Button>
+        <Button variant="secondary" href="https://therealsuji.tk" target="_blank">
+          <ContactRound /> About Me
+        </Button>
+      </div>
+    </div>
+  </Sheet.Content>
+</Sheet.Root>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,6 +5,7 @@
   import Moon from "@lucide/svelte/icons/moon";
   import Sun from "@lucide/svelte/icons/sun";
   import { onMount } from 'svelte';
+  import AboutSheet from '$lib/components/ui/about-sheet/about-sheet.svelte';
 
 	let { children } = $props();
   let isDarkMode = $state(false);
@@ -29,13 +30,16 @@
 <div class="fixed z-1 w-full bg-background/30 backdrop-blur-sm border-b-1">
   <div class="flex items-center justify-between py-4 px-8 mx-auto max-w-200">
     <h1 class="text-2xl">CP Contests</h1>
-    <Button class="cursor-pointer" size="icon" variant="outline" onclick={toggleTheme}>
-      {#if isDarkMode}
-        <Sun />
-      {:else}
-        <Moon />
-      {/if}
-    </Button>
+    <div class="flex gap-2">
+      <Button class="cursor-pointer" size="icon" variant="outline" onclick={toggleTheme}>
+        {#if isDarkMode}
+          <Sun />
+        {:else}
+          <Moon />
+        {/if}
+      </Button>
+      <AboutSheet />
+    </div>
   </div>
 </div>
 
