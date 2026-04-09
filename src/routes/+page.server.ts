ba4c51710710ad721c0be683ffb85e794c723d2d
Sujit Kumar <60378235+therealsujitk@users.noreply.github.com>
2026-04-10 01:51:27 +0530

    fix: group contests using client timezone

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -2,16 +2,22 @@ import type { Contest } from '$lib/interfaces';
 import { getAtCoderContests, getCodeChefContests, getCodeforcesContests, getLeetCodeContests } from '$lib/services/contest';
 import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async () => {
+export const load: PageServerLoad = async ({ request }) => {
   const contests: Contest[] = [];
   const erroredSites: string[] = [];
 
+  // Fetch contests from all sites in parallel and handle errors for each site individually
   await Promise.all([
     getAtCoderContests().then(c => contests.push(...c)).catch(() => erroredSites.push('AtCoder')),
     getCodeChefContests().then(c => contests.push(...c)).catch(() => erroredSites.push('CodeChef')),
     getCodeforcesContests().then(c => contests.push(...c)).catch(() => erroredSites.push('Codeforces')),
     getLeetCodeContests().then(c => contests.push(...c)).catch(() => erroredSites.push('LeetCode')),
   ]);
+
+  // Get the user's timezone from the request headers (set by Vercel) and send it to the client.
+  // This is done to prevent rendering mismatches between server and client due to timezone differences.
+  const serverTimezone = Intl.DateTimeFormat().resolvedOptions().timeZone || 'UTC';
+  const clientTimezone = request.headers.get('x-vercel-ip-timezone') || serverTimezone;
 
 	return {
     contests: contests.toSorted((a, b) => a.startTime.getTime() - b.startTime.getTime()),
@@ -20,6 +26,7 @@ export const load: PageServerLoad = async () => {
         title: 'Failed to fetch contests from one or more sites',
         message: `We were unable to fetch contests from the following sites: ${erroredSites.join(', ')}.`,
       }] : [])
-    ]
+    ],
+    clientTimezone: clientTimezone,
   };
 };
